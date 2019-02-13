e0d8e5805a45da416e7677338b4a85bafa3f654b
unknown <evertescalante@gmail.com>
2019-02-13 18:58:50 -0500

    'ngRoute-with-$routeProvider'

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,18 @@
 (function () {
 	let app = angular.module('Pokedex', [
+		'ngRoute',
 		'pokedex.controllers',
 		'pokedex.directives',
 		'pokedex.filters'
 		])
+
+		app.config(['$routeProvider', function ($routeProvider) {
+
+			$routeProvider
+				.when('/', {
+					templateUrl:'views/pokemon.html',
+					controller:  'PokemonController'
+				})
+		}])
 
 })()
