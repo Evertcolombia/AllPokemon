573e0d720b7c0d6727746f9e0d57fd7d751bf5d0
unknown <evertescalante@gmail.com>
2019-02-13 19:55:43 -0500

    'normalize-filter'

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,5 +1,20 @@
+/*Este archivo contiene dos filtros, uno para normalizar el nombra que contenga caracteres raros,
+retorna ese input en minusculas
+El otro toma el input que le pasemos y contruye una ruta hacia las imaenes de los pokemons
+*/
 (function () {
 	angular.module('pokedex.filters', [])
+		.filter('normalize', function () {
+			return function (input) {
+				input = input
+							.replace('♂', 'm')
+							.replace('♀', 'f')
+							.replace(/\W+/g, '')
+				return input.toLowerCase();
+
+			}
+		})
+
 		.filter('imageify', function () {
 			return function (input) {
 				let url = 'img/pokemons/' + input.toLowerCase() + '.jpg'
